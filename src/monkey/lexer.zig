ff672d18f5ceba803680a7ba72b9d61e591ab26b
dot96gal <dot96gal@gmail.com>
2025-01-08 22:02:02 +0900

    add lexer

--- src/monkey/lexer.zig
+++ src/monkey/lexer.zig
@@ -24,6 +24,57 @@ pub const Lexer = struct {
         _ = self;
     }
 
+    pub fn nextToken(self: *Self) Token {
+        self.skipWhitespace();
+
+        const token: Token = switch (self.ch) {
+            '=' => blk: {
+                if (self.peekChar() == '=') {
+                    self.readChar();
+                    break :blk Token.equal;
+                } else {
+                    break :blk Token.assign;
+                }
+            },
+            '+' => Token.plus,
+            '-' => Token.minus,
+            '!' => blk: {
+                if (self.peekChar() == '=') {
+                    self.readChar();
+                    break :blk Token.not_equal;
+                } else {
+                    break :blk Token.bang;
+                }
+            },
+            '*' => Token.asterisk,
+            '/' => Token.slash,
+            '<' => Token.less_than,
+            '>' => Token.greater_than,
+            ',' => Token.comma,
+            ';' => Token.semicolon,
+            '(' => Token.left_paren,
+            ')' => Token.right_paren,
+            '{' => Token.left_brace,
+            '}' => Token.right_brace,
+            0 => Token.eof,
+            else => {
+                if (self.isLetter(self.ch)) {
+                    const identifier = self.readIdentifier();
+                    return Token.lookup_identifier(identifier);
+                } else if (self.isDigit(self.ch)) {
+                    const number = self.readNumber();
+                    return Token{ .integer = number };
+                } else {
+                    return Token.illegal;
+                }
+            },
+        };
+
+        self.readChar();
+
+        return token;
+    }
+
     fn readChar(self: *Self) void {
         if (self.readPosition >= self.input.len) {
             self.ch = 0;
@@ -34,46 +85,531 @@ pub const Lexer = struct {
         self.readPosition += 1;
     }
 
-    pub fn nextToken(self: *Self) Token {
-        const token: Token = switch (self.ch) {
-            '=' => Token.assign,
-            '+' => Token.plus,
-            ',' => Token.comma,
-            ';' => Token.semicolon,
-            '(' => Token.left_paren,
-            ')' => Token.right_paren,
-            '{' => Token.left_brace,
-            '}' => Token.right_brace,
-            0 => Token.eof,
-            else => unreachable,
+    fn peekChar(self: Self) u8 {
+        if (self.readPosition >= self.input.len) {
+            return 0;
+        } else {
+            return self.input[self.readPosition];
+        }
+    }
+
+    fn readIdentifier(self: *Self) []const u8 {
+        const position = self.position;
+        while (self.isLetter(self.ch)) {
+            self.readChar();
+        }
+
+        return self.input[position..self.position];
+    }
+
+    fn readNumber(self: *Self) []const u8 {
+        const position = self.position;
+        while (self.isDigit(self.ch)) {
+            self.readChar();
+        }
+
+        return self.input[position..self.position];
+    }
+
+    fn skipWhitespace(self: *Self) void {
+        while (self.isWhitespace(self.ch)) {
+            self.readChar();
+        }
+    }
+
+    fn isWhitespace(_: Self, ch: u8) bool {
+        return switch (ch) {
+            ' ', '\t', '\r', '\n' => true,
+            else => false,
         };
+    }
 
-        self.readChar();
+    fn isLetter(_: Self, ch: u8) bool {
+        return switch (ch) {
+            'a'...'z', 'A'...'Z', '_' => true,
+            else => false,
+        };
+    }
 
-        return token;
+    fn isDigit(_: Self, ch: u8) bool {
+        return switch (ch) {
+            '0'...'9' => true,
+            else => false,
+        };
     }
 };
 
-test "test Lexer.nextToken" {
-    const input = "=+,;(){}";
-
-    const exptected_tokens = [_]Token{
-        Token.assign,
-        Token.plus,
-        Token.comma,
-        Token.semicolon,
-        Token.left_paren,
-        Token.right_paren,
-        Token.left_brace,
-        Token.right_brace,
-        Token.eof,
+test "Lexer.nextToken" {
+    const TestCase = struct {
+        input: []const u8,
+        expected_tokens: []const Token,
     };
 
-    var lexer = Lexer.init(input);
-    defer lexer.deinit();
+    const tests: []const TestCase = &.{
+        .{
+            .input = "=+,;(){}",
+            .expected_tokens = &.{
+                Token.assign,
+                Token.plus,
+                Token.comma,
+                Token.semicolon,
+                Token.left_paren,
+                Token.right_paren,
+                Token.left_brace,
+                Token.right_brace,
+                Token.eof,
+            },
+        },
+        .{
+            .input =
+            \\let five = 5;
+            \\let ten = 10;
+            \\
+            \\let add = fn(x, y) {
+            \\  x + y;
+            \\};
+            \\let result = add(five, ten);
+            ,
+            .expected_tokens = &.{
+                Token.let,
+                Token{ .identifier = "five" },
+                Token.assign,
+                Token{ .integer = "5" },
+                Token.semicolon,
+                Token.let,
+                Token{ .identifier = "ten" },
+                Token.assign,
+                Token{ .integer = "10" },
+                Token.semicolon,
+                Token.let,
+                Token{ .identifier = "add" },
+                Token.assign,
+                Token.function,
+                Token.left_paren,
+                Token{ .identifier = "x" },
+                Token.comma,
+                Token{ .identifier = "y" },
+                Token.right_paren,
+                Token.left_brace,
+                Token{ .identifier = "x" },
+                Token.plus,
+                Token{ .identifier = "y" },
+                Token.semicolon,
+                Token.right_brace,
+                Token.semicolon,
+                Token.let,
+                Token{ .identifier = "result" },
+                Token.assign,
+                Token{ .identifier = "add" },
+                Token.left_paren,
+                Token{ .identifier = "five" },
+                Token.comma,
+                Token{ .identifier = "ten" },
+                Token.right_paren,
+                Token.semicolon,
+                Token.eof,
+            },
+        },
+        .{
+            .input =
+            \\!-/*5;
+            \\5 < 10 > 5;
+            ,
+            .expected_tokens = &.{
+                Token.bang,
+                Token.minus,
+                Token.slash,
+                Token.asterisk,
+                Token{ .integer = "5" },
+                Token.semicolon,
+                Token{ .integer = "5" },
+                Token.less_than,
+                Token{ .integer = "10" },
+                Token.greater_than,
+                Token{ .integer = "5" },
+                Token.semicolon,
+                Token.eof,
+            },
+        },
+        .{
+            .input =
+            \\if (5 < 10) {
+            \\    return true;
+            \\} else {
+            \\    return false;
+            \\}
+            ,
+            .expected_tokens = &.{
+                Token.keyword_if,
+                Token.left_paren,
+                Token{ .integer = "5" },
+                Token.less_than,
+                Token{ .integer = "10" },
+                Token.right_paren,
+                Token.left_brace,
+                Token.keyword_return,
+                Token.true,
+                Token.semicolon,
+                Token.right_brace,
+                Token.keyword_else,
+                Token.left_brace,
+                Token.keyword_return,
+                Token.false,
+                Token.semicolon,
+                Token.right_brace,
+                Token.eof,
+            },
+        },
+        .{
+            .input =
+            \\10 == 10;
+            \\10 != 9;
+            ,
+            .expected_tokens = &.{
+                Token{ .integer = "10" },
+                Token.equal,
+                Token{ .integer = "10" },
+                Token.semicolon,
+                Token{ .integer = "10" },
+                Token.not_equal,
+                Token{ .integer = "9" },
+                Token.semicolon,
+                Token.eof,
+            },
+        },
+    };
 
-    for (exptected_tokens) |expected_token| {
-        const token = lexer.nextToken();
-        try std.testing.expectEqual(token, expected_token);
+    for (tests) |tt| {
+        var lexer = Lexer.init(tt.input);
+        defer lexer.deinit();
+
+        for (tt.expected_tokens) |expected_token| {
+            const token = lexer.nextToken();
+            try std.testing.expect(expected_token.equals(token));
+        }
+    }
+}
+
+test "Lexer.readChar" {
+    const TestCase = struct {
+        input: []const u8,
+        expected: []const u8,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = "abc",
+            .expected = &.{
+                'a',
+                'b',
+                'c',
+                0,
+            },
+        },
+        .{
+            .input = "abc d",
+            .expected = &.{
+                'a',
+                'b',
+                'c',
+                ' ',
+                'd',
+                0,
+            },
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init(tt.input);
+        defer lexer.deinit();
+
+        for (tt.expected) |expected_ch| {
+            try std.testing.expectEqual(expected_ch, lexer.ch);
+            lexer.readChar();
+        }
+    }
+}
+
+test "Lexer.peekChar" {
+    const TestCase = struct {
+        input: []const u8,
+        expected: []const u8,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = "abc",
+            .expected = &.{
+                'b',
+                'c',
+                0,
+            },
+        },
+        .{
+            .input = "abc d",
+            .expected = &.{
+                'b',
+                'c',
+                ' ',
+                'd',
+                0,
+            },
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init(tt.input);
+        defer lexer.deinit();
+
+        for (tt.expected) |expected_ch| {
+            const actual = lexer.peekChar();
+            try std.testing.expectEqual(expected_ch, actual);
+            lexer.readChar();
+        }
+    }
+}
+
+test "Lexer.readIdentifier" {
+    const TestCase = struct {
+        input: []const u8,
+        expected: []const u8,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = "let",
+            .expected = "let",
+        },
+        .{
+            .input = "fn",
+            .expected = "fn",
+        },
+        .{
+            .input = "five",
+            .expected = "five",
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init(tt.input);
+        defer lexer.deinit();
+
+        const actual = lexer.readIdentifier();
+        try std.testing.expectEqual(tt.expected, actual);
+    }
+}
+
+test "Lexer.readNumber" {
+    const TestCase = struct {
+        input: []const u8,
+        expected: []const u8,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = "1",
+            .expected = "1",
+        },
+        .{
+            .input = "12",
+            .expected = "12",
+        },
+        .{
+            .input = "123",
+            .expected = "123",
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init(tt.input);
+        defer lexer.deinit();
+
+        const actual = lexer.readNumber();
+        try std.testing.expectEqual(tt.expected, actual);
+    }
+}
+
+test "Lexer.skipWhitespace" {
+    const TestCase = struct {
+        input: []const u8,
+        expected: []const u8,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = "abc",
+            .expected = &.{
+                'a',
+                'b',
+                'c',
+                0,
+            },
+        },
+        .{
+            .input = "  a \t b \r c \n d  ",
+            .expected = &.{
+                'a',
+                'b',
+                'c',
+                'd',
+                0,
+            },
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init(tt.input);
+        defer lexer.deinit();
+
+        for (tt.expected) |expected_ch| {
+            lexer.skipWhitespace();
+            try std.testing.expectEqual(expected_ch, lexer.ch);
+            lexer.readChar();
+        }
+    }
+}
+
+test "Lexer.isWhitespace" {
+    const TestCase = struct {
+        input: u8,
+        expected: bool,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = ' ',
+            .expected = true,
+        },
+        .{
+            .input = '\t',
+            .expected = true,
+        },
+        .{
+            .input = '\n',
+            .expected = true,
+        },
+        .{
+            .input = '\r',
+            .expected = true,
+        },
+
+        .{
+            .input = 'a',
+            .expected = false,
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init("");
+        defer lexer.deinit();
+
+        const actual = lexer.isWhitespace(tt.input);
+        try std.testing.expectEqual(tt.expected, actual);
+    }
+}
+test "Lexer.isLetter" {
+    const TestCase = struct {
+        input: u8,
+        expected: bool,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = 'a',
+            .expected = true,
+        },
+        .{
+            .input = 'z',
+            .expected = true,
+        },
+        .{
+            .input = 'A',
+            .expected = true,
+        },
+        .{
+            .input = 'Z',
+            .expected = true,
+        },
+
+        .{
+            .input = '_',
+            .expected = true,
+        },
+        .{
+            .input = '0',
+            .expected = false,
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init("");
+        defer lexer.deinit();
+
+        const actual = lexer.isLetter(tt.input);
+        try std.testing.expectEqual(tt.expected, actual);
+    }
+}
+
+test "Lexer.isDigit" {
+    const TestCase = struct {
+        input: u8,
+        expected: bool,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = '0',
+            .expected = true,
+        },
+        .{
+            .input = '1',
+            .expected = true,
+        },
+        .{
+            .input = '2',
+            .expected = true,
+        },
+        .{
+            .input = '3',
+            .expected = true,
+        },
+
+        .{
+            .input = '4',
+            .expected = true,
+        },
+        .{
+            .input = '5',
+            .expected = true,
+        },
+        .{
+            .input = '6',
+            .expected = true,
+        },
+        .{
+            .input = '7',
+            .expected = true,
+        },
+        .{
+            .input = '8',
+            .expected = true,
+        },
+
+        .{
+            .input = '9',
+            .expected = true,
+        },
+        .{
+            .input = 'a',
+            .expected = false,
+        },
+    };
+
+    for (tests) |tt| {
+        var lexer = Lexer.init("");
+        defer lexer.deinit();
+
+        const actual = lexer.isDigit(tt.input);
+        try std.testing.expectEqual(tt.expected, actual);
     }
 }
--- src/monkey/token.zig
+++ src/monkey/token.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 const TokenType = enum {
     illegal,
     eof,
@@ -9,6 +11,14 @@ const TokenType = enum {
     // Operator
     assign, // =
     plus, // +
+    minus, // -
+    bang, // !
+    asterisk, // *
+    slash, // /
+    less_than, // <
+    greater_than, // >
+    equal, // ==
+    not_equal, // !=
 
     // Delimiters
     comma, // ,
@@ -19,8 +29,13 @@ const TokenType = enum {
     right_brace, // }
 
     // Keyword
-    function,
-    let,
+    function, // fn
+    let, // let
+    true, // true
+    false, // false
+    keyword_if, // if
+    keyword_else, // else
+    keyword_return, // return
 };
 
 pub const Token = union(TokenType) {
@@ -34,6 +49,14 @@ pub const Token = union(TokenType) {
     // Operator
     assign: void,
     plus: void,
+    minus: void,
+    bang: void,
+    asterisk: void,
+    slash: void,
+    less_than: void,
+    greater_than: void,
+    equal: void,
+    not_equal: void,
 
     // Delimiters
     comma: void,
@@ -46,4 +69,143 @@ pub const Token = union(TokenType) {
     // Keyword
     function: void,
     let: void,
+    true: void,
+    false: void,
+    keyword_if: void,
+    keyword_else: void,
+    keyword_return: void,
+
+    const Self = @This();
+
+    fn literal(self: Self) []const u8 {
+        return switch (self) {
+            .identifier => self.identifier,
+            .integer => self.integer,
+            else => "",
+        };
+    }
+
+    fn equalsType(self: Self, other: Self) bool {
+        return @as(TokenType, self) == @as(TokenType, other);
+    }
+
+    fn equalsLiteral(self: Self, other: Self) bool {
+        return std.mem.eql(u8, self.literal(), other.literal());
+    }
+
+    pub fn equals(self: Self, other: Self) bool {
+        return self.equalsType(other) and self.equalsLiteral(other);
+    }
+
+    pub fn lookup_identifier(identifier: []const u8) Self {
+        if (std.mem.eql(u8, identifier, "let")) {
+            return Token.let;
+        } else if (std.mem.eql(u8, identifier, "fn")) {
+            return Token.function;
+        } else if (std.mem.eql(u8, identifier, "true")) {
+            return Token.true;
+        } else if (std.mem.eql(u8, identifier, "false")) {
+            return Token.false;
+        } else if (std.mem.eql(u8, identifier, "if")) {
+            return Token.keyword_if;
+        } else if (std.mem.eql(u8, identifier, "else")) {
+            return Token.keyword_else;
+        } else if (std.mem.eql(u8, identifier, "return")) {
+            return Token.keyword_return;
+        } else {
+            return Token{ .identifier = identifier };
+        }
+    }
 };
+
+test "Token.equals" {
+    const TestCase = struct {
+        input_a: Token,
+        input_b: Token,
+        expected: bool,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input_a = Token.illegal,
+            .input_b = Token.illegal,
+            .expected = true,
+        },
+        .{
+            .input_a = Token.illegal,
+            .input_b = Token.eof,
+            .expected = false,
+        },
+        .{
+            .input_a = Token{ .identifier = "abc" },
+            .input_b = Token{ .identifier = "abc" },
+            .expected = true,
+        },
+        .{
+            .input_a = Token{ .identifier = "abc" },
+            .input_b = Token{ .identifier = "def" },
+            .expected = false,
+        },
+        .{
+            .input_a = Token{ .integer = "123" },
+            .input_b = Token{ .integer = "123" },
+            .expected = true,
+        },
+        .{
+            .input_a = Token{ .integer = "123" },
+            .input_b = Token{ .integer = "456" },
+            .expected = false,
+        },
+    };
+
+    for (tests) |tt| {
+        try std.testing.expectEqual(tt.expected, tt.input_a.equals(tt.input_b));
+    }
+}
+
+test "Token.lookup_identifier" {
+    const TestCase = struct {
+        input: []const u8,
+        expected: Token,
+    };
+
+    const tests: []const TestCase = &.{
+        .{
+            .input = "let",
+            .expected = Token.let,
+        },
+        .{
+            .input = "fn",
+            .expected = Token.function,
+        },
+        .{
+            .input = "true",
+            .expected = Token.true,
+        },
+        .{
+            .input = "false",
+            .expected = Token.false,
+        },
+        .{
+            .input = "if",
+            .expected = Token.keyword_if,
+        },
+        .{
+            .input = "else",
+            .expected = Token.keyword_else,
+        },
+        .{
+            .input = "return",
+            .expected = Token.keyword_return,
+        },
+        .{
+            .input = "abc",
+            .expected = Token{ .identifier = "abc" },
+        },
+    };
+
+    for (tests) |tt| {
+        const actual = Token.lookup_identifier(tt.input);
+        try std.testing.expect(tt.expected.equals(actual));
+    }
+}
